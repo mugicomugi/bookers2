d398473e62386e922463d8bc06a83f2b99daeffc
EC2 Default User <127326435+mugicomugi@users.noreply.github.com>
2023-04-12 13:04:24 +0000

    bookers2

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
+  get 'homes/top'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+
+end
